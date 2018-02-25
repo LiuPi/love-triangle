f2434ec2ddb68ccb4f0411fa4f31bcdbdb9fd8d1
Liudmila <mizuxira@yandex.ru>
2018-02-25 23:59:20 +0300

    love-triangle/feature: implement function wich counts love triangles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
+ * @param arr - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var count = 0;
+
+    let n = preferences.length;
+    var triangles = [];
+
+    for (var i = 1; i < n; i++) {
+        if (triangles.includes(i)!= true) {
+            var first = i;
+            var second = preferences[i - 1];
+                       if(first != second) {
+                var third = preferences[second - 1];
+                if ((second != third) && (preferences[third - 1] == first)) {
+                    count++;
+                    triangles.push(second);
+                    triangles.push(third);
+
+                }
+            }
+
+        }
+    }
+
+
+    return count;
+
 };
